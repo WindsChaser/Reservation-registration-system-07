a2537ed61cc78369c090c2071c46b88f8ba1123b
Winds <599149192@qq.com>
2015-11-30 00:17:08 +0800

    框架 version=1.0

---
+++ server/DBService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Threading;
+
+namespace server
+{
+	public class DBService
+	{
+		
+	}
+}
---
+++ server/Entities.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+	public enum Access
+	{
+		unknown,
+		unregistered,
+		registered,
+		manager,
+		system
+	}
+
+	public class User
+	{
+		public Access access
+		{
+			get; set;
+		}
+	}
+
+	public class ReUser : User
+	{
+		public String password;
+		public String userName;
+		public int id;
+		public String realName;
+		public long phoneNumber;
+		public String IDCard;
+		public int credit;
+	}
+
+	public class SytManager : User
+	{
+		public String password;
+		public String userName;
+		public int id;
+	}
+
+	public class Hospital
+	{
+		public int id;
+		public String name;
+		public String address;
+		public String remark;
+		public int rank;
+	}
+
+	public class Department
+	{
+		public int id;
+		public String name;
+		public String _class;
+		public String remark;
+	}
+
+	public class Doctor
+	{
+		public int id;
+		public String name;
+		public String title;
+		public Hospital hospital;
+		public Department department;
+		public String remark;
+	}
+
+	public class reservationList
+	{
+		public int id;
+		public Hospital hospital;
+		public Department department;
+		public Doctor doctor;
+		public DateTime time;
+		public double fee;
+	}
+}
